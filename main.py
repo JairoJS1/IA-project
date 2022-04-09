88366f14475eb38a6210e741422e787f8c8d6284
JairoJS1 <jairo.j199912@gmail.com>
2022-04-08 20:42:29 -0600

    Primer commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Escuchando...")
+    audio = r.listen(source)
+
+    try:
+        text = r.recognize_google(audio, language='es-GT')
+        print("Lo que se dijo: {}".format(text))
+    except:
+        print("Error al escuchar")
